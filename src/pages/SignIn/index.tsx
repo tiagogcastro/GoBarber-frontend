a46af9623f883a83d78724dea0e6eb8a5f2a2f8a
Tiaguin061 <tiaguin180@gmail.com>
2021-03-08 16:10:09 -0300

    feat: Login using context API

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -13,14 +13,19 @@ import { Background, Container, Content } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
-import AuthContext from '../../context/AuthContext';
+import {AuthContext} from '../../context/AuthContext';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { name } = useContext(AuthContext);
+  const { name, signIn } = useContext(AuthContext);
 
-  const handleSubmit = useCallback( async (data: object) => {
+  const handleSubmit = useCallback( async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -36,6 +41,10 @@ const SignIn: React.FC = () => {
         abortEarly: false
       });
 
+      signIn({
+        email: data.email,
+        password: data.password
+      });
     } catch (err) {
       console.log(err);
 
@@ -43,7 +52,7 @@ const SignIn: React.FC = () => {
 
       formRef.current?.setErrors(errors);
     }
-  }, []);
+  }, [signIn]);
 
   return (
     <Container>
