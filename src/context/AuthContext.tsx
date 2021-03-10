ab7889b7d65175331ca9d53d0fe4843cb17818b4
Tiaguin061 <tiaguin180@gmail.com>
2021-03-10 17:48:23 -0300

    feat: creating useAuth hook

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 import api from '../services/api';
 
 interface AuthState {
@@ -16,7 +16,7 @@ interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({children}) => {
   const [data, setData] = useState<AuthState>(() => {
@@ -52,3 +52,14 @@ export const AuthProvider: React.FC = ({children}) => {
     </AuthContext.Provider>
   );
 }
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if(!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+} 
+
