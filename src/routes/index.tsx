5bc3a47f1d92f648c6a982b53cccb3adbe878a96
Tiaguin061 <tiaguin180@gmail.com>
2021-05-20 09:23:13 -0300

    feat: Added router for forgot password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,12 +7,14 @@ import Route from './routes';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component ={SignIn} />
       <Route path="/signup" component ={SignUp} />
+      <Route path="/forgot-password" exact component ={ForgotPassword} />
 
       <Route path="/dashboard" component ={Dashboard} isPrivate/>
     </Switch>
