6050d3767fef2307b85a76da24ee75dd531aba82
Tiaguin061 <tiaguin180@gmail.com>
2021-05-21 20:54:41 -0300

    fix: Setting loading as optional

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import React, { ButtonHTMLAttributes } from 'react';
 import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  loading: boolean;
+  loading?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({children, loading, ...rest}) => (
