b46330f7d44a5b358c2e9b31ac39aee1f24adb51
Tiaguin061 <tiaguin180@gmail.com>
2021-05-26 15:04:44 -0300

    feat: Added default headers

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,6 +31,7 @@ export const AuthProvider: React.FC = ({children}) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return {
         token,
         user: JSON.parse(user)
@@ -50,6 +51,8 @@ export const AuthProvider: React.FC = ({children}) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({token, user});
   },[])
